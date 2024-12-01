04676d02cedafcea6ce1151242c1bf3d6f5fa466
JamesAri <James.Ari@outlook.com>
2024-12-01 16:53:12 +0100

    removed worker from riot api service, added decorator for rate limiting, small refactor of the service

--- src/riot_api_service.py
+++ src/riot_api_service.py
@@ -1,7 +1,7 @@
-import asyncio
+from functools import wraps
 import logging
 
-import utils
+from utils.requests import construct_query_params
 import config
 
 logger = logging.getLogger(__name__)
@@ -11,30 +11,34 @@ class RiotApiService:
     def __init__(self, session):
         self.session = session
 
-    async def GET_request(self, resource):
+    def rate_limited(func):
+        @wraps(func)
+        async def wrapper(self, *args, **kwargs):
+            print("TODO: Implement rate limiting")
+            return await func(self, *args, **kwargs)
+        return wrapper
+
+    @rate_limited
+    async def _GET(self, resource):
         headers = {"X-Riot-Token": config.secrets['api_key']}
         async with self.session.get(url=resource, headers=headers) as response:
-            return await response.json()
+            logger.debug(f"[>] GET {response.url}")
+            res = await response.json()
+            if response.status != 200:
+                raise Exception(f"Error: {res}")
+            return res
 
     async def get_matches(self, startTime=None, endTime=None, queue=None, type=None, start=None, count=None):
         arguments = {**locals()}
         del arguments['self']
-        query_params = utils.construct_query_params(**arguments)
+        query_params = construct_query_params(**arguments)
         resource = f"/lol/match/v5/matches/by-puuid/{config.secrets['puuid']}/ids" + query_params
-        return await self.GET_request(resource=resource)
+        return await self._GET(resource=resource)
 
     async def get_match_statistics(self, match_id):
         resource = f"/lol/match/v5/matches/{match_id}"
-        return await self.GET_request(resource=resource)
+        return await self._GET(resource=resource)
 
-    async def worker(name, queue):
-        while True:
-            # Get a "work item" out of the queue.
-            sleep_for = await queue.get()
-
-            # Sleep for the "sleep_for" seconds.
-            await asyncio.sleep(sleep_for)
-
-            # Notify the queue that the "work item" has been processed.
-            queue.task_done()
-            logging.debug(f'[^] worker {name} has slept for {sleep_for:.2f} seconds')
+    async def get_match_end_timestamp(self, match_id) -> int:
+        stats = await self.get_match_statistics(match_id)
+        return int(stats['info']['gameEndTimestamp'])
